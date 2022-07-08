8acc682390fc5576888062c1c852d38b24237c1b
Jitendra <JItendra@example.com>
2022-07-09 03:09:48 +0530

    ok

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_dns_managed_zone" "example-zone" {
+resource "google_dns_managed_zone" "cloud-dns" {
   name        = "Bruttech-zone"
-  dns_name    = "Bruttech-${random_id.rnd.hex}.com."
+  dns_name    = "Bruttech- ${random_id.rnd.hex}.com."
   description = "Cloud DNS Module"
   labels = {
     foo = "bar"
