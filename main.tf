2bf62e73bf2a23feaee2570dff3a6101e863bcde
jeetuapth <63745741+jeetuapth@users.noreply.github.com>
2022-07-08 20:19:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  project = "{{jitendralab}}"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
 resource "google_dns_managed_zone" "example-zone" {
   name        = "example-zone"
   dns_name    = "example-${random_id.rnd.hex}.com."
