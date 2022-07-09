53bc86a643544c7436c2cf01a7c390264925fdb8
Jitendra <JItendra@example.com>
2022-07-10 00:25:13 +0530

    k

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ resource "google_compute_router" "cloud_router" {
   bgp {
     asn               = var.asn
     advertise_mode    = var.advertise_mode
-    advertised_groups = ["ALL_SUBNETS"]
-    advertised_ip_ranges {
+    advertised_groups = ["null"]
+       advertised_ip_ranges {
       range = "10.0.10.0/29"
     }
     advertised_ip_ranges {
