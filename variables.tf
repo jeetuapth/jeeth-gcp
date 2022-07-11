74569ec1bdbb2f93feecf9237a894744f5e9485f
Jitendra <JItendra@example.com>
2022-07-11 19:34:29 +0530

    j

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "project" {
+  type    = string
+  default = "jitendralab"
+}
+
+variable "region" {
+  type    = string
+  default = "asia-south1"
+}
+
+variable "zone" {
+  type    = string
+  default = "asia-south1-c"
+}
+
+
+variable "networks_name" {
+  type    = string
+  default = "jeeth-network"
+}
+
+variable "service_port_name" {
+  type    = number
+  default = "80"
+}
+
+variable "service_port" {
+  type    = number
+  default = "80"
+}
+
+variable "backend" {
+  type    = map
+  default = "null"
+}
