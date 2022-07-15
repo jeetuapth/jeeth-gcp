1d3a5d090bd7cc14933a23e5ec809820979cce1c
Jitendra <JItendra@example.com>
2022-07-16 03:41:45 +0530

    ok

--- main.tf
+++ main.tf
@@ -10,6 +10,19 @@ module "network" {
   #zone = "us-central1-a"
   app_name	= var.app_name
   private_subnet_cidr_1	=	var.private_subnet_cidr_1
+}
+
+module "network_Subnet_2" {
+  source  = "app.terraform.io/Bruttech/network/google"
+  version = "2.0.3"
+  # insert required variables here
+  project = var.project
+  region  = var.region
+  zone = var.zone
+  #project = "jitendralab"
+  #region  = "us-central1"
+  #zone = "us-central1-a"
+  app_name	= var.app_name
   private_subnet_cidr_1	=	var.private_subnet_cidr_2
 }
 
