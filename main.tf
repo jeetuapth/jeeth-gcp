5c5580164e0070e751470a0d93afdaaf0494257f
Jitendra <JItendra@example.com>
2022-07-16 21:26:09 +0530

    Map

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/Bruttech/network/google"
-  version = "2.0.3"
+  version = "2.0.5"
   # insert required variables here
   project = var.project
   region  = var.region
@@ -9,9 +9,13 @@ module "network" {
   #region  = "us-central1"
   #zone = "us-central1-a"
   app_name	= var.app_name
-  private_subnet_cidr_1	=	var.private_subnet_cidr_1
-}
+  #private_subnet_cidr_1	=	var.private_subnet_cidr_1
+  private-subnet  = var.private-subnet
+  private_subnet_cidr = var.private_subnet_cidr
 
+ }
+
+/*
 module "network_Subnet_2" {
   source  = "app.terraform.io/Bruttech/network/google"
   version = "2.0.3"
@@ -26,3 +30,4 @@ module "network_Subnet_2" {
   private_subnet_cidr_1	=	var.private_subnet_cidr_2
 }
 
+*/
--- variables.tf
+++ variables.tf
@@ -18,17 +18,23 @@ variable "zone" {
   default     = "us-central1-a"
 }
 
+/*
 # define private subnet
 variable "private_subnet_cidr_1" {
   type = string
   description = "private_subnet_CIDR 1"
   default     = "10.0.10.0/28"
 }
-
-variable "private_subnet_cidr_2" {
-  type = string
-  description = "private_subnet_CIDR 2"
-  default     = "10.0.11.0/28"
+*/
+variable "private-subnet" {
+  description = "Create private subnets with these names"
+  type        = list(string)
+  default     = ["web-subnet", "app-subnet", "db-subnet"]
+}
+variable "private_subnet_cidr" {
+  description = "Create private subnets cidr with these names"
+  type        = list(string)
+  default     = ["10.0.10.0/28", "10.0.11.0/28", "10.0.12.0/28"]
 }
 
 # define application name
